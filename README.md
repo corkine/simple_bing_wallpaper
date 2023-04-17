119725bbba6180d0efaca2c5e1e351def196e6e8
mazj <mazhangjing@inspur.com>
2023-04-17 10:42:52 +0800

    更改了命令行参数逻辑，现提供自动解析获取 URL 方式

--- README.md
+++ README.md
@@ -16,4 +16,12 @@
 - Most Wayland compositors (set only, requires swaybg)
 - i3 (set only, requires feh)
 
-特性：可传入自定义的壁纸 URL，默认使用托管的 4K 中国区壁纸源。当设置过当日壁纸后，下次调用程序将直接退出，以在移动设备（MacBook 等）设备配合 crontab 使用。
+特性：可传入自定义的壁纸 URL（包括了一个远程壁纸 URL 地址），不传入参数则默认自解析以获取 4K 中国区壁纸源。当设置过当日壁纸后，下次调用程序将直接退出，以在移动设备（MacBook 等）设备配合 crontab 使用。
+
+```bash
+# parse get url from cn.bing.com
+bing_wallpaper
+# or download image from url
+bing_wallpaper https://YOUR-WALLPAPER-URL-HERE
+bing_wallpaper default-server
+```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
 use wallpaper;
 use chrono;
+use reqwest;
+use serde_json;
+
+fn parse_get_url() -> String {
+    let resp = reqwest::blocking::get("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1").unwrap();
+    let json: serde_json::Value = serde_json::from_str(&resp.text().unwrap()).unwrap();
+    let images = json["images"].as_array().unwrap();
+    let image = images[0].as_object().unwrap();
+    let url = image["urlbase"].as_str().unwrap();
+    let url = format!("https://www.bing.com{}_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&rs=1&c=4&qlt=100&uhd=1&uhdwidth=3840&uhdheight=2160", url);
+    return url;
+}
+
+#[test]
+fn test_parse_get_url() {
+    let url = parse_get_url();
+    println!("{}", url);
+    assert!(url.starts_with("https://www.bing.com"));
+    assert!(url.ends_with("_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&rs=1&c=4&qlt=100&uhd=1&uhdwidth=3840&uhdheight=2160"));
+}
 
 fn main() {
-    let mut _url = "https://go.mazhangjing.com/bing-today-image";
+    let mut _url: String = "".to_string();
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         if args[1] == "-h" || args[1] == "--help" {
-            println!("Usage: download and set wallpaper from a url (or default), rm *.set to download again");
+            println!("Usage: download and set wallpaper from a url (or `default-server`), 
+       rm *.set to download again.
+       pass nothing to auto parse url from cn.bing.com");
             return;
         } else {
-            _url = &args[1];
+            let content = &args[1];
+            if content.eq_ignore_ascii_case("default-server") {
+                _url = "https://go.mazhangjing.com/bing-today-image".to_string();
+            } else {
+                _url = content.to_string();
+            }
         }
+    }
+    if _url.is_empty() {
+        _url = parse_get_url();
     }
     println!("setting wallpaper from {}", _url);
     //get date of now, format as 20200101
@@ -22,7 +52,7 @@ fn main() {
         return;
     } 
     //set wallpaper
-    wallpaper::set_from_url(_url).unwrap();
+    wallpaper::set_from_url(_url.as_str()).unwrap();
     wallpaper::set_mode(wallpaper::Mode::Crop).unwrap();
     std::fs::File::create(format!("{}.set", date)).unwrap();
 }
